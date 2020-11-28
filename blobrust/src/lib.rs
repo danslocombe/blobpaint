a331ec4b12249166c25b54dabe3a32efd8575012
root <root@DESKTOP-0OC4HDH.localdomain>
2020-11-28 19:34:09 +0000

    Sample from dirty region on canvas to improve responsiveness

--- blobrust/src/lib.rs
+++ blobrust/src/lib.rs
@@ -5,7 +5,7 @@ extern crate rand_core;
 extern crate rand_xorshift;
 
 mod utils;
-mod data_doublebuffer;
+mod pointdata;
 mod brush;
 
 use std::f32::consts::TAU;
@@ -17,7 +17,7 @@ use rand_xorshift::{XorShiftRng};
 
 
 use utils::{rand_unit};
-use data_doublebuffer::DataDoubleBuffer;
+use pointdata::{PointData, PointDataStore, DirtyRect};
 use brush::{Brush};
 
 const MAX_UNDOS : usize = 8;
@@ -57,37 +57,20 @@ pub struct BlobCanvas {
   thresh_base : f32,
   thresh_t_var : f32,
   thresh_t_mult : f32,
-  data : DataDoubleBuffer<PointData>,
+  data : PointDataStore,
   undo_stack : VecDeque<Vec<PointData>>,
   draw_buffer: Vec<DrawPoint>,
   t : u32,
   rng: XorShiftRng,
 }
 
-impl BlobCanvas {
-  fn get_index(&self, x: u32, y: u32) -> usize {
-    (y * self.width + x) as usize
-  }
-
-  fn try_get_index(&self, x: i32, y: i32) -> Option<usize> {
-    if x < 0 || x >= self.width as i32 || y < 0 || y >= self.height as i32 {
-      None
-    }
-    else {
-      Some((y as u32 * self.width + x as u32) as usize)
-    }
-  }
-}
-
 static RAND_SEED : [u8; 16] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-
 
 #[wasm_bindgen]
 impl BlobCanvas {
   pub fn new(width : u32, height: u32) -> Self {
-    let size = width * height;
     let mut rng = XorShiftRng::from_seed(RAND_SEED);
-    let canvas_data = empty_canvas_data(size as usize, &mut rng);
+    let canvas_data = empty_canvas_data(width, height, &mut rng);
 
     let pixels_updated_per_frame : usize = ((width as f32) * (height as f32) * 0.05) as usize;
     let mut draw_buffer = Vec::with_capacity(pixels_updated_per_frame);
@@ -119,13 +102,13 @@ impl BlobCanvas {
     let t_y_var = t + TAU * (y as f32) / self.height as f32;
 
     let thresh = self.thresh_base + self.thresh_t_var * (t_y_var).sin();
-    let i = self.get_index(x, y);
+    let i = self.data.get_index(x, y);
     let point_data = self.data.get_imm()[i];
     point_data.sample(&mut self.rng, thresh, 0.05)
   }
 
-  pub fn fill_draw_buffer(&mut self) {
-    for i in 0..self.draw_buffer.len() {
+  fn fill_draw_buffer_uniform(&mut self, count : usize) {
+    for i in 0..count {
       let x = self.rng.next_u32() % self.width;
       let y = self.rng.next_u32() % self.height;
       let col = self.sample_pixel(x, y);
@@ -135,6 +118,39 @@ impl BlobCanvas {
         col : col
       }
     }
+  }
+
+  pub fn fill_draw_buffer(&mut self) {
+    // Check to see if pointdata was written to in the last frame
+    let rect = self.data.get_last_dirty().clone();
+    if rect.is_clean() {
+      // If not sample uniformly over the entire canvas
+      self.fill_draw_buffer_uniform(self.draw_buffer.len());
+    }
+    else {
+      // Otherwise reserve some of the buffer to fill with data from the dirty
+      // region.
+
+      // TODO take the ratio using the relative sizes of the dirty rect and
+      // the overall canvas.
+      let count = ((self.draw_buffer.len() as f32) * 0.95) as usize;
+      self.fill_draw_buffer_uniform(count);
+
+      // We know this is safe to do because of is_clean() check failing.
+      let x_range = rect.max_x - rect.min_x;
+      let y_range = rect.max_y - rect.min_y;
+
+      for i in count..self.draw_buffer.len() {
+        let x = rect.min_x + (self.rng.next_u32() % x_range);
+        let y = rect.min_y + (self.rng.next_u32() % y_range);
+        let col = self.sample_pixel(x, y);
+        self.draw_buffer[i] = DrawPoint {
+          x : x as u8,
+          y : y as u8,
+          col : col
+        }
+      }
+    };
   }
 
   pub fn get_draw_buffer_size(&self) -> usize {
@@ -158,7 +174,7 @@ impl BlobCanvas {
   pub fn try_pop_undo(&mut self) -> bool {
     match self.undo_stack.pop_back() {
       Some(data) => {
-        self.data = DataDoubleBuffer::new(data);
+        self.data = PointDataStore::new(data, self.width, self.height);
         true
       },
       _ => false,
@@ -167,7 +183,7 @@ impl BlobCanvas {
 
   pub fn clear(&mut self) {
     self.push_undo();
-    self.data = empty_canvas_data(self.data.len(), &mut self.rng)
+    self.data = empty_canvas_data(self.width, self.height, &mut self.rng)
   }
 
   pub fn set_thresh_base(&mut self, val : f32) {
@@ -208,6 +224,13 @@ impl BlobCanvas {
     let y_min = (py - rad).max(0);
     let y_max = (py + rad).min(self.height as i32);
 
+    self.data.set_dirty(DirtyRect {
+      min_x: x_min as u32,
+      min_y: y_min as u32,
+      max_x: x_max as u32,
+      max_y: y_max as u32,
+    });
+
     for y in y_min..y_max {
       for x in x_min..x_max {
         let api = CanvasApi {
@@ -223,13 +246,14 @@ impl BlobCanvas {
   }
 }
 
-fn empty_canvas_data(size: usize, rng: &mut dyn RngCore) -> DataDoubleBuffer<PointData> {
+fn empty_canvas_data(width: u32, height: u32, rng: &mut dyn RngCore) -> PointDataStore {
+    let size = width * height;
     let mut data = Vec::with_capacity(size as usize);
     for _i in 0..size {
       let col = rand_unit(rng);
       data.push(PointData::new(0.0, col));
     }
-    DataDoubleBuffer::new(data)
+    PointDataStore::new(data, width, height)
 }
 
 pub struct CanvasApi<'t> {
@@ -240,48 +264,14 @@ pub struct CanvasApi<'t> {
 
 impl<'t> CanvasApi<'t>{
   pub fn get_mut(&mut self) -> &mut PointData {
-    let i = self.canvas.get_index(self.x, self.y);
+    let i = self.canvas.data.get_index(self.x, self.y);
     &mut self.canvas.data.get_mut()[i]
   }
 
   pub fn try_get_point(&self, xoff : i32, yoff: i32) -> Option<PointData> {
     let x = self.x as i32 + xoff;
     let y = self.y as i32 + yoff;
-    self.canvas.try_get_index(x, y)
+    self.canvas.data.try_get_index(x, y)
       .map(|i| self.canvas.data.get_imm()[i])
-  }
-}
-
-#[derive(Clone, Copy, Debug)]
-pub struct PointData {
-  thresh_band : f32,
-  color_band : f32,
-}
-
-impl PointData {
-  fn new(thresh_band : f32, color_band : f32) -> Self {
-    PointData {
-      thresh_band: thresh_band,
-      color_band: color_band,
-    }
-  }
-
-  fn sample(&self, rng: &mut dyn RngCore, thresh : f32, thresh_band : f32) -> Color {
-    if (self.thresh_band - thresh).abs() < thresh_band {
-      return Color::THRESH;
-    }
-
-    if self.thresh_band > thresh {
-      let seed = rand_unit(rng);
-      if self.color_band < seed {
-        Color::X
-      }
-      else {
-        Color::Y
-      }
-    }
-    else {
-      Color::Z
-    }
   }
 }
---
+++ blobrust/src/pointdata.rs
@@ -0,0 +1,153 @@
+use std::ptr;
+use rand_core::{RngCore};
+
+use super::Color;
+use super::utils::rand_unit;
+
+#[derive(Clone, Copy, Debug)]
+pub struct PointData {
+  pub thresh_band : f32,
+  pub color_band : f32,
+}
+
+impl PointData {
+  pub fn new(thresh_band : f32, color_band : f32) -> Self {
+    PointData {
+      thresh_band: thresh_band,
+      color_band: color_band,
+    }
+  }
+
+  pub fn sample(&self, rng: &mut dyn RngCore, thresh : f32, thresh_band : f32) -> Color {
+    if (self.thresh_band - thresh).abs() < thresh_band {
+      return Color::THRESH;
+    }
+
+    if self.thresh_band > thresh {
+      let seed = rand_unit(rng);
+      if self.color_band < seed {
+        Color::X
+      }
+      else {
+        Color::Y
+      }
+    }
+    else {
+      Color::Z
+    }
+  }
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug, Default)]
+pub struct DirtyRect {
+    pub min_x : u32,
+    pub max_x : u32,
+    pub min_y : u32,
+    pub max_y : u32,
+}
+
+impl DirtyRect {
+    pub fn is_clean(&self) -> bool {
+        self.max_x < self.min_x && self.max_y < self.min_y
+    }
+
+    pub fn clean_rect(width : u32, height : u32) -> Self {
+        DirtyRect {
+            min_x : width,
+            min_y : height,
+            max_x : 0,
+            max_y : 0,
+        }
+    }
+}
+
+/// Contains a mutable and immutable copy of the same data.
+/// 
+/// We use this to provide an interface to brushes. The brush can
+/// read data from the immutable buffer representing data in the previous frame,
+/// and write to the mutable data to prepare the next frame.
+pub struct PointDataStore {
+    mut_data : Vec<PointData>,
+    imm_data : Vec<PointData>,
+    width : u32,
+    height : u32,
+    dirty : DirtyRect,
+    dirty_last : DirtyRect,
+}
+
+impl PointDataStore {
+    pub fn new(data : Vec<PointData>, width : u32, height : u32) -> Self {
+
+        let dirty = DirtyRect::clean_rect(width as u32, height as u32);
+        let dirty_last = dirty.clone();
+
+        PointDataStore {
+            mut_data: data.clone(),
+            imm_data: data,
+            width : width as u32,
+            height : height as u32,
+            dirty : dirty,
+            dirty_last : dirty_last,
+        }
+    }
+
+    pub fn get_index(&self, x: u32, y: u32) -> usize {
+        (y * self.width + x) as usize
+    }
+
+    pub fn try_get_index(&self, x: i32, y: i32) -> Option<usize> {
+        if x < 0 || x >= self.width as i32 || y < 0 || y >= self.height as i32 {
+            None
+        }
+        else {
+            Some((y as u32 * self.width + x as u32) as usize)
+        }
+    }
+
+    pub fn get_mut(&mut self) -> &mut [PointData] {
+        &mut self.mut_data
+    }
+
+    pub fn set_dirty(&mut self, rect : DirtyRect) {
+        self.dirty = rect
+    }
+
+    #[allow(dead_code)]
+    pub fn get_dirty(&self) -> &DirtyRect {
+        &self.dirty
+    }
+
+    pub fn get_last_dirty(&self) -> &DirtyRect {
+        &self.dirty_last
+    }
+
+    pub fn get_imm(&self) -> &[PointData] {
+        &self.imm_data
+    }
+
+    pub fn get_clone(&self) -> Vec<PointData> {
+        self.mut_data.clone()
+    }
+
+    /// Flush the mutable data and copy it into the immutable buffer.
+    pub fn flush(&mut self) {
+        // Only do work if there is a section that needs to be copied.
+        if !self.dirty.is_clean() {
+            unsafe {
+                // Iterate over the section with changes calling memcpy on each line within the
+                // changed region.
+                let len = self.dirty.max_x - self.dirty.min_x;
+                for y in self.dirty.min_y..self.dirty.max_y {
+                    let offset = self.get_index(self.dirty.min_x, y);
+                    let src_ptr = self.mut_data.as_ptr().wrapping_add(offset); 
+                    let dst_ptr = self.imm_data.as_mut_ptr().wrapping_add(offset);
+
+                    ptr::copy_nonoverlapping(src_ptr, dst_ptr, len as usize);
+                }
+            }
+        }
+
+        self.dirty_last = self.dirty;
+        self.dirty = DirtyRect::clean_rect(self.width, self.height)
+    }
+}
